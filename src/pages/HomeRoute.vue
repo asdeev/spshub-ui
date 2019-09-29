e173d6428f81f7c6940cf7a53b401cbf0625ef49
Abdinur Mohamed <noor1me@uw.edu>
2019-09-28 20:02:56 -0700

    New home page added, All images removed

--- src/pages/HomeRoute.vue
+++ src/pages/HomeRoute.vue
@@ -1,13 +1,45 @@
+
 <template>
   <!-- vue:test -->
-  <v-carousel class="home-route__carousel" hide-controls height="48.5em">
-    <v-carousel-item
-      class="home-route__images"
-      v-for="(item, index) in home.carousel"
-      :key="item.id"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
+  <v-container>
+    <v-layout justify-center align-center> </v-layout>
+    <v-layout row wrap justify-space-between>
+      <v-flex md8>
+        <v-layout column wrap justify-space-between>
+          <v-flex>
+            <p class="display-2 font-weight-light">Somali Professionals & Students Hub </p>
+            <h4 class="display-3 font-italic" style="color:navy">
+              <br>
+               Website Under Construction
+            </h4>
+          </v-flex>
+          <v-flex >
+            <br>
+            <h2 class="display-3 font-weight-light" style="color:green">Join Us:</h2>
+          </v-flex>
+          <v-flex offset-md1 class="spacing-top">
+            <h2>
+          <p>
+          <a href ="https://docs.google.com/forms/d/e/1FAIpQLSd3vE6_d8QhskuTtvRM2Dt5ZmH4rUUhMV9iNZQWuVKL2JGZXQ/viewform">Became a Mentee/Mentor</a>
+          </p>
+          <p></p>
+          <p>
+            <a href ="https://docs.google.com/forms/d/e/1FAIpQLSdIuFJY8PVekGp9r0DPNnLD_eSXiq3iyEBaLW4Io7C1yn_rKA/viewform">Become a Mentor</a> 
+          </p>
+             
+            </h2>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex md4>
+        <v-img
+          src="/assets/images/flayer3.jpeg"
+          max-height="600"
+          max-width="600"
+        ></v-img>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
