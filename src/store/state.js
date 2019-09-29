3cce2a8d461643b61bf0792b63f9fa8b2b3072b4
Abdinur Mohamed <noor1me@uw.edu>
2019-09-28 18:24:53 -0700

    School children picture added

--- src/store/state.js
+++ src/store/state.js
@@ -73,8 +73,8 @@ export default {
       { id: 0, src: './assets/images/education.jpg' },
       { id: 1, src: './assets/images/elephant-herd.jpg' },
       { id: 2, src: './assets/images/seattle-night.jpg' },
-      { id: 3, src: './assets/images/baby-camel.jpg' }
-     // { id: 4, src: './assets/images/school-children.jpg' }
+      { id: 3, src: './assets/images/baby-camel.jpg' },
+      { id: 4, src: './assets/images/school-children.jpg' }
     ]
   }
 };
